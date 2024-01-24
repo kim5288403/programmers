69cba73ee392e575e3cd4f67cbb460db8abb2f8c
Lim jihyeok <76669119+kim5288403@users.noreply.github.com>
2024-01-24 15:08:29 +0900

    [level 3] Title: 스티커 모으기(2), Time: 58.28 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -5,12 +5,10 @@ def solution(sticker):
         return sticker[-1]
     
     dp1 = [0] + sticker[:-1]
+    dp2 = [0] + sticker[1:]
+    
     for i in range(2, len(sticker)):
         dp1[i] = max(dp1[i - 1], dp1[i - 2] + dp1[i])
-        
-        
-    dp2 = [0] + sticker[1:]
-    for i in range(2, len(sticker)):
         dp2[i] = max(dp2[i - 1], dp2[i - 2] + dp2[i])
         
     return max(dp1[-1], dp2[-1])
