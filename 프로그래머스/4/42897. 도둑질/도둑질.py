5e2faa5d94c9a79f75191d66fdd741166e3acc6a
Lim jihyeok <76669119+kim5288403@users.noreply.github.com>
2024-01-24 15:07:40 +0900

    [level 4] Title: 도둑질, Time: 539.10 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,14 +1,10 @@
 def solution(money):
     
     dp1 = [0] + money[:-1]
-    
+    dp2 = [0] + money[1:]
     
     for i in range(2, len(money)):
         dp1[i] = max(dp1[i - 1], dp1[i - 2] + dp1[i])
-        
-    dp2 = [0] + money[1:]
-    for i in range(2, len(money)):
         dp2[i] = max(dp2[i - 1], dp2[i - 2] + dp2[i])
         
-    
     return max(dp1[-1], dp2[-1])
