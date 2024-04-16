c68c4452db5dc435cd2b4b888b047c535b0f0e7e
Lim jihyeok <76669119+kim5288403@users.noreply.github.com>
2024-04-16 14:14:46 +0900

    [Gold IV] Title: 치즈, Time: 84 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,23 +1,32 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 
 dy = [0, 0, 1, -1]
 dx = [1, -1, 0, 0]
 
-def dfs(y, x):
+def bfs(y, x):
+    q = deque()
+    q.append((y, x))
+    v[y][x] = True
 
-    if arr[y][x] == 1:
-        c_arr.append((y, x))
-        return
+    while q:
+        qy, qx = q.popleft()
 
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 0 <= ny < N and 0 <= nx < M:
-            if not v[ny][nx]:
-                v[ny][nx] = True
-                dfs(ny, nx)
+        if arr[qy][qx] == 1:
+            c_arr.append((qy, qx))
+            continue
+
+        for i in range(4):
+            ny, nx = qy + dy[i], qx + dx[i]
+            if 0 <= ny < N and 0 <= nx < M:
+                if not v[ny][nx]:
+                    v[ny][nx] = True
+                    q.append((ny, nx))
+
+
+
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
@@ -27,7 +36,7 @@ while True:
     count_b = 0
     c_arr = []
     v = [[False] * M for _ in range(N)]
-    dfs(0, 0)
+    bfs(0, 0)
     count_a += 1
 
     for y, x in c_arr:
