f89c140616725d7a99fe8097ad05578b95a272e9
Lim jihyeok <76669119+kim5288403@users.noreply.github.com>
2024-04-22 16:08:05 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 124 ms, Memory: 35328 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -13,11 +13,11 @@ def bfs(x):
         if s == K:
             print(v[s])
             break
-        
+
         if 0 <= s - 1 < limit and v[s - 1] == -1:
             v[s - 1] = v[s] + 1
             q.append(s - 1)
-        if 0 < s * 2 < limit and v[s * 2] == -1:
+        if 0 <= s * 2 < limit and v[s * 2] == -1:
             v[s * 2] = v[s]
             q.appendleft(s * 2)  # 2* 가 다른 연산보다 더 높은 우선순위를 가지기 위함
         if 0 <= s + 1 < limit and v[s + 1] == -1:
